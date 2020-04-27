65650544a0f184566b0b7402410daeb6888649ea
gustavo <gustavo.maia@b2b.com.pt>
2020-04-27 17:11:17 +0100

    Adding a RecyclerView to a Fragment

--- app/src/main/java/com/artemissoftware/afroditedating/HomeFragment.java
+++ app/src/main/java/com/artemissoftware/afroditedating/HomeFragment.java
@@ -5,17 +5,38 @@ import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.artemissoftware.afroditedating.adapters.MainRecyclerViewAdapter;
+import com.artemissoftware.afroditedating.models.User;
+import com.artemissoftware.afroditedating.util.Users;
+
+import java.util.ArrayList;
 
 
 public class HomeFragment extends Fragment {
 
     private static final String TAG = "HomeFragment";
+
+
+    //constants
+    private static final int NUM_COLUMNS = 2;
+
+    //widgets
+    private RecyclerView mRecyclerView;
+
+    //vars
+    private ArrayList<User> mMatches = new ArrayList<>();
+    private StaggeredGridLayoutManager mStaggeredGridLayoutManager;
+    private MainRecyclerViewAdapter mRecyclerViewAdapter;
+
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -24,7 +45,34 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         Log.d(TAG, "onCreateView: started.");
+        mRecyclerView = view.findViewById(R.id.recycler_view);
+
+        findMatches();
         return view;
     }
 
+
+    private void findMatches() {
+
+        Users users = new Users();
+
+        if (mMatches != null) {
+            mMatches.clear();
+        }
+        for (User user : users.USERS) {
+            mMatches.add(user);
+        }
+        if (mRecyclerViewAdapter == null) {
+            initRecyclerView();
+        }
+    }
+
+    private void initRecyclerView(){
+        Log.d(TAG, "initRecyclerView: init recyclerview.");
+        mStaggeredGridLayoutManager = new StaggeredGridLayoutManager(NUM_COLUMNS, LinearLayoutManager.VERTICAL);
+        mRecyclerView.setLayoutManager(mStaggeredGridLayoutManager);
+        //mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        mRecyclerViewAdapter = new MainRecyclerViewAdapter(getActivity(), mMatches);
+        mRecyclerView.setAdapter(mRecyclerViewAdapter);
+    }
 }
---
+++ app/src/main/java/com/artemissoftware/afroditedating/IMainActivity.java
@@ -0,0 +1,9 @@
+package com.artemissoftware.afroditedating;
+
+
+import com.artemissoftware.afroditedating.models.User;
+
+public interface IMainActivity {
+
+    void inflateViewProfileFragment(User user);
+}
