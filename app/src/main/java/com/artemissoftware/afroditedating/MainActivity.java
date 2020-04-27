1b896d41bd8e0aa9f3f46464bedb1c1ca5733fcc
gustavo <gustavo.maia@b2b.com.pt>
2020-04-27 12:58:26 +0100

    Implementing an Alert Dialog

--- app/src/main/java/com/artemissoftware/afroditedating/MainActivity.java
+++ app/src/main/java/com/artemissoftware/afroditedating/MainActivity.java
@@ -1,14 +1,55 @@
 package com.artemissoftware.afroditedating;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.artemissoftware.afroditedating.util.PreferenceKeys;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        isFirstLogin();
+    }
+
+    private void isFirstLogin(){
+        Log.d(TAG, "isFirstLogin: checking if this is the first login");
+
+        final SharedPreferences preferences = this.getSharedPreferences(getString(R.string.app_name), Context.MODE_PRIVATE);
+        boolean isFirstLogin = preferences.getBoolean(PreferenceKeys.FIRST_TIME_LOGIN, true);
+
+        if(isFirstLogin){
+            Log.d(TAG, "isFirstLogin: lauching alert dialog");
+
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setMessage(getString(R.string.first_time_user_message));
+            alertDialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.d(TAG, "onClick: closing dialog");
+
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putBoolean(PreferenceKeys.FIRST_TIME_LOGIN, false);
+                    editor.commit();
+                    dialog.dismiss();
+                }
+            });
+
+            alertDialogBuilder.setIcon(R.drawable.tabian_dating);
+            alertDialogBuilder.setTitle(" ");
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+
+        }
     }
 }
